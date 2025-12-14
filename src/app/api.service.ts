70f75262240f08ce4c1106874191704c7bae5346
Hashory <89839285+Hashory@users.noreply.github.com>
2025-12-15 04:04:05 +0900

    feat: add ApiService for Hono client integration with SSR support

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformServer } from '@angular/common';
 import { hc } from 'hono/client';
-import { AppType } from '../hono';
+import { app, AppType } from '../hono';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +9,9 @@ import { AppType } from '../hono';
 export class ApiService {
   private platformId = inject(PLATFORM_ID);
 
-  // For SSR, an absolute URL is required. In the browser, a relative URL works.
-  // For the development environment, specify localhost:4200 on the server side.
-  private baseUrl = isPlatformServer(this.platformId)
-    ? 'http://localhost:4200'
-    : '/';
-
-  client = hc<AppType>(this.baseUrl);
+  client = hc<AppType>('/', {
+    fetch: isPlatformServer(this.platformId)
+      ? (input: any, init?: any) => app.request(input, init)
+      : undefined
+  });
 }
