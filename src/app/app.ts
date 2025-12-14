28475dccc67ad2d92d9dc939a5fc5b5fa74eeb0a
Hashory <89839285+Hashory@users.noreply.github.com>
2025-12-14 18:44:26 +0900

    feat: Add fetching during on the cloudflare worker SSR

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-hono-worker');
+
+  private http = inject(HttpClient);
+  // This data fetching is also executed during SSR (on Cloudflare Worker)
+  protected readonly todoData = toSignal(this.http.get('https://jsonplaceholder.typicode.com/todos/1'));
 }
