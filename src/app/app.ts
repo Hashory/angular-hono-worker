e0785dc0e6a4f867beef8a5804807c17d1609909
Hashory <89839285+Hashory@users.noreply.github.com>
2025-12-15 04:11:13 +0900

    feat: add initial App component with remote data fetching and SSR transfer state

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal, inject, TransferState, makeStateKey, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, isPlatformServer } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { from } from 'rxjs';
 import { ApiService } from './api.service';
@@ -17,16 +17,31 @@ export class App {
 
   private http = inject(HttpClient);
   private api = inject(ApiService);
+  private transferState = inject(TransferState);
+  private platformId = inject(PLATFORM_ID);
 
   // This data fetching is also executed during SSR (on Cloudflare Worker)
   protected readonly todoData = toSignal(this.http.get('https://jsonplaceholder.typicode.com/todos/1'));
 
   // Hono Client Data
   protected readonly honoData = toSignal(
-    from(
-      this.api.client.hono
-        .$get({ query: { name: 'Visitor' } })
-        .then((res) => res.text())
-    )
+    from(this.getHonoData())
   );
+
+  private async getHonoData() {
+    const key = makeStateKey<string>('HONO_DATA');
+
+    if (this.transferState.hasKey(key)) {
+      return this.transferState.get(key, '');
+    }
+
+    const res = await this.api.client.hono.$get({ query: { name: 'Visitor' } });
+    const text = await res.text();
+
+    if (isPlatformServer(this.platformId)) {
+      this.transferState.set(key, text);
+    }
+
+    return text;
+  }
 }
